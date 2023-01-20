65065c71608d8c8a1f8ea3eef5c5907129498a06
VMM-MMV <121231490+VMM-MMV@users.noreply.github.com>
2023-01-20 12:17:10 +0200

    Mihai almost end

--- src/mihai.py
+++ src/mihai.py
@@ -14,11 +14,8 @@ def limit(i):
     for k in range(len(i)-1):
         if i[k].isalpha() and i[k+1].isnumeric():
             phrase = i[:k+2]
-            print(phrase)
             try:
                 m = i[k+2:].split()[0]
-                
-                print(m)
                 if m.lower() in pieces:
                     return ((i[:k+2],m))
                 else:
@@ -27,93 +24,68 @@ def limit(i):
                 return ((i[:k+2],""))
     return li
 
-def checkCastle(i,x,j):
-  if j=="castle":
-            print("$")
-            if i[x-6:x-1]=="short":
-              return("O-O")
-            elif i[x-5:x-1]=="long":
-              return("O-O-O")
-  else:
-    return("")
 
 def form(li):
     vlad_op_list = []
     final = []
     phrase = ""
-    # print(li)
     for i in li:
         for j in i.split():
             x = lev(j)
-            # castle=checkCastle(x[1],j)
-            # if castle!="":
-            #   return(castle)
-
             if x[0] >= 80:
-                # print(x)
                 l = i.index(j)
                 i=i[:l]+x[2]+i[l+len(j):]
                 tup=limit(i[l:])
-                print(tup)
                 vlad_op_list.append((tup[0],tup[1]))
 
     if ('','') in vlad_op_list:
         vlad_op_list.remove(('',''))
     return(vlad_op_list)
 def voice():
-    #s = "w"
-    # use the microphone as source for input.
       with sr.Microphone() as source2:
-      # wait for a second to let the recognizer
-      # adjust the energy threshold based on
-      # the surrounding noise level
         r.adjust_for_ambient_noise(source2, duration=0.1)
-      
-      #listens for the user's input
+
         audio2 = r.listen(source2)
-      
-      # Using google to recognize audio
-        MyText = r.recognize_google(audio2); #print(MyText)
-        # MyText=[{'transcript': 'sadawd rook to E4 Queen'}, {'transcript': ' night to E4'},] 
-      #MyText = MyText.lower()print(MyText)
-      #print(MyText)
-            #s = "1"
-            
-      #SpeakText(MyText)
 
-
-    #MyText=[{'transcript': 'sadawd night to E4', 'confidence': 0.86834723}, {'transcript': ' sawwwastrrs nice to E4'}, {'transcript': 'Knight to E4'}, {'transcript': 
-    #'night to e-4'}, {'transcript': 'night to eat for'}, {'transcript': ' how r u? rook to B8'}, {'transcript': 'hahaha CHECKMATE queen to C4'}]
+        MyText = r.recognize_google(audio2)
 
       li = [i.get("transcript") for i in MyText] 
-      li=form(li)
 
-      print(li)
-  
-      #print(sorted(my_second_cool_list, reverse=True)[:8])
-
-
-      #print(sorted(my_second_cool_list)[0][2])
-
-      right = li[0]
-      print(right)
-      v = ""
-      match (right[0].split()[0]):
-        case "knight":
-          v = ("n" + right[0][-2:]) if "to" in right[0] else ("nx" + right[0][-2:])
-        case "queen":
-          v = ("q" + right[0][-2:]) if "to" in right[0] else ("qx" + right[0][-2:])
-        case "rook":
-          v = ("r" + right[0][-2:]) if "to" in right[0] else ("rx" + right[0][-2:])
-        case "king":
-          v = ("k" + right[0][-2:]) if "to" in right[0] else ("kx" + right[0][-2:])
-        case "bishop":
-          v = ("b" + right[0][-2:]) if "to" in right[0] else ("bx" + right[0][-2:])
-        case "pawn":
-          v = ("p" + right[0][-2:]) if "to" in right[0] else ("px" + right[0][-2:])
-      if right[1].lower() in pieces:
-        v+=right[1][:1]
-      return(v)
+      print(li, "li")
+      return li
+      
+cas = 0
+li = voice()
+for i in li:
+        print(i)
+        if "castle" in i.lower():
+                cas += 1
+print(form(li))
+v = ""
+if cas == 0:
+        right = form(li)[0]
+        match (right[0].split()[0]):
+                case "knight":
+                  v = ("n" + right[0][-2:]) if "to" in right[0] else ("nx" + right[0][-2:])
+                case "queen":
+                  v = ("q" + right[0][-2:]) if "to" in right[0] else ("qx" + right[0][-2:])
+                case "rook":
+                  v = ("r" + right[0][-2:]) if "to" in right[0] else ("rx" + right[0][-2:])
+                case "king":
+                  v = ("k" + right[0][-2:]) if "to" in right[0] else ("kx" + right[0][-2:])
+                case "bishop":
+                  v = ("b" + right[0][-2:]) if "to" in right[0] else ("bx" + right[0][-2:])
+                case "pawn":
+                  v = ("p" + right[0][-2:]) if "to" in right[0] else ("px" + right[0][-2:])
+        if right[1].lower() in pieces:
+          v+=right[1][:1]
+else:
+        for i in li:
+                if "long" in i:
+                        v = "O-O-O"
+                        break
+                elif "short" in i:
+                        v = "O-O"
+                        break
+print(v)
     
-  
-voice()
